5660a57671ff27e8b78d16a7220f2e6fd564f05e
Spencer <spencerschlott@yahoo.com>
2022-03-28 16:26:31 -0400

    Initial check-in of module Ch09Ex1NFLTeams

---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NFLTeams.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var session = new NFLSession(HttpContext.Session);
+            var model = new TeamListViewModel
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams(),
+                UserName = session.GetName()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Change(TeamListViewModel model)
+        {
+            var session = new NFLSession(HttpContext.Session);
+            session.SetName(model.UserName);
+            return RedirectToAction("Index", "Home", new
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv()
+            });
+        }
+    }
+}
